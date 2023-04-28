f9cbb398f980e102dac996e945f77b7b995df31d
Gerardo Garcia <gerrex95@gmail.com>
2023-04-28 17:17:37 -0600

    agrege sql plugins y mobile en pagina nosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
+import { NosotrosComponent } from './nosotros/nosotros/nosotros.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     pathMatch: 'full'
+  },
+  {
+    path: 'nosotros',
+    component: NosotrosComponent,
   },
   {
     path: '**',
