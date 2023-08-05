decfe933416d25d5bb3718c880e6e68adcc6b492
Gerardo Garcia <gerrex95@gmail.com>
2023-08-04 18:35:29 -0600

    agrege boton con funcionalidades y colores

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,61 +10,61 @@ export class HomeComponent {
   categories = [
   {
     name: 'Oficina',
-    img: '../../../assets/img/png/home/categories/bizhub-C3351-office.jpg'
+    img: '../../../assets/img/home/categories/bizhub-C3351-office.jpg'
   },
   {
     name: 'Producción',
-    img:'../../../assets/img/png/home/categories/konica-c2060-production.jpg'
+    img:'../../../assets/img/home/categories/konica-c2060-production.jpg'
   },
   {
     name: 'Inyección de Tinta',
-    img:'../../../assets/img/png/home/categories/epson-wf-m5799-ink.jpg'
+    img:'../../../assets/img/home/categories/epson-wf-m5799-ink.jpg'
   },
   {
     name: 'Consumibles',
-    img:'../../../assets/img/png/home/categories/TN_319-removebg-preview.png'
+    img:'../../../assets/img/home/categories/TN_319-removebg-preview.png'
   },
   {
     name: 'Etiquetas',
-    img:'../../../assets/img/png/home/categories/c6000a-epson-labels.jpg'
+    img:'../../../assets/img/home/categories/c6000a-epson-labels.jpg'
   },
   {
     name: 'Plotters',
-    img:'../../../assets/img/png/home/categories/epson-plotter.jpg'
+    img:'../../../assets/img/home/categories/epson-plotter.jpg'
   },
   {
     name: 'Artes Gráficas',
-    img:'../../../assets/img/png/home/categories/bizhub-pro-1060.jpg'
+    img:'../../../assets/img/home/categories/bizhub-pro-1060.jpg'
   }];
 
   clients = [
     {
       name: 'LG',
-      svg: '../../../assets/img/png/home/logo-clientes/Blanco/lg-electronics-white.svg'
+      svg: '../../../assets/img/home/logo-clientes/Blanco/lg-electronics-white.svg'
     },
     {
       name: 'Risoul',
-      svg:'../../../assets/img/png/home/logo-clientes/Blanco/risoul-white.svg'
+      svg:'../../../assets/img/home/logo-clientes/Blanco/risoul-white.svg'
     },
     {
       name: 'Ryasa',
-      svg:'../../../assets/img/png/home/logo-clientes/Blanco/ryasa-white.svg'
+      svg:'../../../assets/img/home/logo-clientes/Blanco/ryasa-white.svg'
     },
     {
       name: 'Semex',
-      svg:'../../../assets/img/png/home/logo-clientes/Blanco/semex-white.svg'
+      svg:'../../../assets/img/home/logo-clientes/Blanco/semex-white.svg'
     },
     {
       name: 'TecMilenio',
-      svg:'../../../assets/img/png/home/logo-clientes/Blanco/universidad-tecmilenio-white.svg'
+      svg:'../../../assets/img/home/logo-clientes/Blanco/universidad-tecmilenio-white.svg'
     },
     {
       name: 'Grupo Vitro',
-      svg:'../../../assets/img/png/home/logo-clientes/Blanco/vitro-white.svg'
+      svg:'../../../assets/img/home/logo-clientes/Blanco/vitro-white.svg'
     },
     {
       name: 'Thomas & Betts',
-      svg:'../../../assets/img/png/home/logo-clientes/Blanco/thomas-betts-white.svg'
+      svg:'../../../assets/img/home/logo-clientes/Blanco/thomas-betts-white.svg'
   }];
 
 
