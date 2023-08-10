201cf4551f94232a03108d2351d28bdaf23faa0e
Gerardo Garcia <gerrex95@gmail.com>
2023-08-09 18:39:35 -0600

    agregando logica a componente filtro

--- src/app/productos/components/filter/filter.component.ts
+++ src/app/productos/components/filter/filter.component.ts
@@ -1,5 +1,7 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Printer } from '../../interfaces/printer.interface';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-filter',
@@ -21,9 +23,101 @@ import { Component } from '@angular/core';
     ])
   ]
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit {
+  @Output() filteredPrintersChange = new EventEmitter<Printer[]>(); // Output event
+  // FILTERS
+  printers: Printer[] = [];
+  filteredPrinters: Printer[] = [];
+  selectedPrintSizeFilters: String[] = [];
+  printSizesFilter: String[] = ['13" x 47', 'Banner', 'Legal', 'Super-B', 'A4', 'Tabloide', 'Tabloide +'];
+  selectedColors: String[] = [];
+  colorFilter: String[] = ['Color', 'B&N'];
+  selectedBrands: String[] = [];
+  brands: String[] = ['Konica Minolta', 'Kyocera', 'Epson'];
+  selectedRentableOptions: boolean[] = [false, true];
+  rentableOptions: boolean[] = [false, true];
+
   filterSectionsState = 'open';
   checked = false;
+
+
+  constructor(private printerService: ProductosService) {}
+
+  ngOnInit(): void {
+    this.printerService.getPrinters().subscribe((data) => {
+      this.printers = data;
+      this.filteredPrinters = data;
+    })
+  }
+
+  applyFilters(): void {
+    if (this.selectedPrintSizeFilters.length > 0) {
+      this.filteredPrinters = this.printers.filter((printer) =>
+        this.selectedPrintSizeFilters.includes(printer.maxPrintSizeSimple)
+      );
+    } else {
+      this.filteredPrinters = this.printers;
+    }
+
+    if (this.selectedColors.length > 0) {
+      this.filteredPrinters = this.filteredPrinters.filter((printer) =>
+        this.selectedColors.includes(printer.color ? 'Color' : 'B&N')
+      );
+    }
+    
+    if (this.selectedBrands.length > 0) {
+      this.filteredPrinters = this.filteredPrinters.filter((printer) =>
+        this.selectedBrands.includes(printer.brand)
+      );
+    }
+
+    if (this.selectedRentableOptions.length > 0) {
+      this.filteredPrinters = this.filteredPrinters.filter((printer) =>
+        this.selectedRentableOptions.includes(printer.rentable)
+      );
+    }
+
+    this.filteredPrintersChange.emit(this.filteredPrinters);
+    console.log(this.filteredPrinters)
+  }
+
+  togglePrintSizeFilter(printSize: String): void {
+    if (this.selectedPrintSizeFilters.includes(printSize)) {
+      this.selectedPrintSizeFilters = this.selectedPrintSizeFilters.filter((b) => b !== printSize);
+    } else {
+      this.selectedPrintSizeFilters.push(printSize);
+    }
+    this.applyFilters();
+  }
+
+  toggleColorFilter(color: String): void {
+    // Toggle color filters
+    if (this.selectedColors.includes(color)) {
+      this.selectedColors = this.selectedColors.filter((c) => c !== color);
+    } else {
+      this.selectedColors.push(color);
+    }
+    this.applyFilters(); // Apply filters after toggling
+  }
+
+  toggleBrandFilter(brand: String): void {
+    if (this.selectedBrands.includes(brand)) {
+      this.selectedBrands = this.selectedBrands.filter((b) => b !== brand);
+    } else {
+      this.selectedBrands.push(brand);
+    }
+    this.applyFilters();
+  }
+
+  toggleRentableOptionFilter(option: boolean): void {
+    if (this.selectedRentableOptions.includes(option)) {
+      this.selectedRentableOptions = this.selectedRentableOptions.filter((o) => o !== option);
+    } else {
+      this.selectedRentableOptions.push(option);
+    }
+    this.applyFilters();
+  }
+  
 
   toggleFilterSections() {
     this.filterSectionsState = this.filterSectionsState === 'open' ? 'closed' : 'open';
--- src/app/productos/pipes/brand-type.pipe.ts
+++ src/app/productos/pipes/brand-type.pipe.ts
@@ -8,17 +8,25 @@ export class BrandTypePipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
   transform(brandName: string): SafeHtml {
     let imagePath: string;
+    let imgWidth: string = "100px"
+    let imgHeight: string = "25px"
     
     // Map the brand names to their respective image paths
     switch (brandName.toLowerCase()) {
       case 'konica minolta':
         imagePath = '../../../assets/svg/home/proveedores/konica_minolta_logo.svg';
+        imgWidth = '165px';
+        imgHeight = 'auto';
         break;
       case 'kyocera':
         imagePath = '../../../assets/svg/home/proveedores/kyocera.svg';
+        imgWidth = '125px';
+        imgHeight = 'auto';
         break;
       case 'epson':
         imagePath = '../../../assets/svg/home/proveedores/epson.svg';
+        imgWidth = '100px';
+        imgHeight = 'auto';
         break;
       default:
         // Return a default image path or handle unsupported brand names as needed
@@ -26,7 +34,7 @@ export class BrandTypePipe implements PipeTransform {
     }
 
     // Return the HTML img tag with the appropriate image path
-    const imgTag = `<img src="${imagePath}" alt="${brandName} Logo" style="width: 180px; height: 25px;">`;
+    const imgTag = `<img src="${imagePath}" alt="${brandName} Logo" style="width: ${imgWidth}; height:${imgHeight};">`;
 
     // Mark the HTML as safe
     return this.sanitizer.bypassSecurityTrustHtml(imgTag);
