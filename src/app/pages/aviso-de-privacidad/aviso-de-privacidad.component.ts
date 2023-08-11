2d55dff329450c48e2d95de2577a631ea6b4534a
Gerardo Garcia <gerrex95@gmail.com>
2023-08-11 15:02:11 -0600

    cree y termine pagina de aviso de privacidad

---
+++ src/app/pages/aviso-de-privacidad/aviso-de-privacidad.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aviso-de-privacidad',
+  templateUrl: './aviso-de-privacidad.component.html',
+  styleUrls: ['./aviso-de-privacidad.component.scss']
+})
+export class AvisoDePrivacidadComponent {
+
+}
