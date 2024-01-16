000940780db3cc26088574dc28cb08f024e3edf1
Jonas Clausen <jonastclausen@gmail.com>
2024-01-16 17:18:16 -0600

    fix accent bug
    Accent caused routing from home.component.ts to not find the correct printers. Taken away for now. Needs implementation later. Also formatting with prettier :accept:

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,14 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Thumbs, SwiperOptions, Autoplay } from 'swiper';
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Thumbs,
+  SwiperOptions,
+  Autoplay,
+} from 'swiper';
 
 // install Swiper modules
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Thumbs, Autoplay]);
@@ -13,66 +21,68 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Thumbs, Autoplay]);
 })
 export class HomeComponent {
   thumbsSwiper: any;
-  
+
   categories = [
-  {
-    name: 'Oficina',
-    img: '../../../assets/img/home/categories/bizhub-C3351-office.jpg'
-  },
-  {
-    name: 'Produccion',
-    img:'../../../assets/img/home/categories/konica-c2060-production.jpg'
-  },
-  {
-    name: 'Inyección de Tinta',
-    img:'../../../assets/img/home/categories/epson-wf-m5799-ink.jpg'
-  },
-  {
-    name: 'Consumibles',
-    img:'../../../assets/img/home/categories/TN_319-removebg-preview.png'
-  },
-  {
-    name: 'Etiquetas',
-    img:'../../../assets/img/home/categories/c6000a-epson-labels.jpg'
-  },
-  {
-    name: 'Plotters',
-    img:'../../../assets/img/home/categories/epson-plotter.jpg'
-  },
-  {
-    name: 'Artes Gráficas',
-    img:'../../../assets/img/home/categories/bizhub-pro-1060.jpg'
-  }];
+    {
+      name: 'Oficina',
+      img: '../../../assets/img/home/categories/bizhub-C3351-office.jpg',
+    },
+    {
+      name: 'Produccion',
+      img: '../../../assets/img/home/categories/konica-c2060-production.jpg',
+    },
+    {
+      name: 'Inyeccion de Tinta',
+      img: '../../../assets/img/home/categories/epson-wf-m5799-ink.jpg',
+    },
+    {
+      name: 'Consumibles',
+      img: '../../../assets/img/home/categories/TN_319-removebg-preview.png',
+    },
+    {
+      name: 'Etiquetas',
+      img: '../../../assets/img/home/categories/c6000a-epson-labels.jpg',
+    },
+    {
+      name: 'Plotters',
+      img: '../../../assets/img/home/categories/epson-plotter.jpg',
+    },
+    {
+      name: 'Artes Graficas',
+      img: '../../../assets/img/home/categories/bizhub-pro-1060.jpg',
+    },
+  ];
 
   clients = [
     {
       name: 'LG',
-      svg: '../../../assets/img/home/logo-clientes/Blanco/lg-electronics-white.svg'
+      svg: '../../../assets/img/home/logo-clientes/Blanco/lg-electronics-white.svg',
     },
     {
       name: 'Risoul',
-      svg:'../../../assets/img/home/logo-clientes/Blanco/risoul-white.svg'
+      svg: '../../../assets/img/home/logo-clientes/Blanco/risoul-white.svg',
     },
     {
       name: 'Ryasa',
-      svg:'../../../assets/img/home/logo-clientes/Blanco/ryasa-white.svg'
+      svg: '../../../assets/img/home/logo-clientes/Blanco/ryasa-white.svg',
     },
     {
       name: 'Semex',
-      svg:'../../../assets/img/home/logo-clientes/Blanco/semex-white.svg'
+      svg: '../../../assets/img/home/logo-clientes/Blanco/semex-white.svg',
     },
     {
       name: 'TecMilenio',
-      svg:'../../../assets/img/home/logo-clientes/Blanco/universidad-tecmilenio-white.svg'
+      svg: '../../../assets/img/home/logo-clientes/Blanco/universidad-tecmilenio-white.svg',
     },
     {
       name: 'Grupo Vitro',
-      svg:'../../../assets/img/home/logo-clientes/Blanco/vitro-white.svg'
+      svg: '../../../assets/img/home/logo-clientes/Blanco/vitro-white.svg',
     },
     {
       name: 'Thomas & Betts',
-      svg:'../../../assets/img/home/logo-clientes/Blanco/thomas-betts-white.svg'
-  }];
+      svg: '../../../assets/img/home/logo-clientes/Blanco/thomas-betts-white.svg',
+    },
+  ];
 
   config: SwiperOptions = {
     slidesPerView: 1,
@@ -122,33 +132,33 @@ export class HomeComponent {
       {
         breakpoint: '1400px',
         numVisible: 4,
-        numScroll: 1
+        numScroll: 1,
       },
       {
-          breakpoint: '1200px',
-          numVisible: 3,
-          numScroll: 1
+        breakpoint: '1200px',
+        numVisible: 3,
+        numScroll: 1,
       },
       {
-          breakpoint: '992px',
-          numVisible: 2,
-          numScroll: 1
+        breakpoint: '992px',
+        numVisible: 2,
+        numScroll: 1,
       },
       {
-          breakpoint: '768px',
-          numVisible: 1,
-          numScroll: 1
+        breakpoint: '768px',
+        numVisible: 1,
+        numScroll: 1,
       },
       {
-          breakpoint: '576px',
-          numVisible: 1,
-          numScroll: 1
+        breakpoint: '576px',
+        numVisible: 1,
+        numScroll: 1,
       },
       {
-          breakpoint: '0px',
-          numVisible: 1,
-          numScroll: 1
-      }
+        breakpoint: '0px',
+        numVisible: 1,
+        numScroll: 1,
+      },
     ];
 
     this.responsiveClientOptions = [
@@ -163,43 +173,42 @@ export class HomeComponent {
         numScroll: 0,
       },
       {
-          breakpoint: '1200px',
-          numVisible: 5,
-          numScroll: 1,
-          circular: false
+        breakpoint: '1200px',
+        numVisible: 5,
+        numScroll: 1,
+        circular: false,
       },
       {
-          breakpoint: '992px',
-          numVisible: 4,
-          numScroll: 1
+        breakpoint: '992px',
+        numVisible: 4,
+        numScroll: 1,
       },
       {
-          breakpoint: '768px',
-          numVisible: 4,
-          numScroll: 1
+        breakpoint: '768px',
+        numVisible: 4,
+        numScroll: 1,
       },
       {
-          breakpoint: '576px',
-          numVisible: 3,
-          numScroll: 1
+        breakpoint: '576px',
+        numVisible: 3,
+        numScroll: 1,
       },
       {
-          breakpoint: '375px',
-          numVisible: 2,
-          numScroll: 1
+        breakpoint: '375px',
+        numVisible: 2,
+        numScroll: 1,
       },
       {
-          breakpoint: '265px',
-          numVisible: 1,
-          numScroll: 1
-      }
+        breakpoint: '265px',
+        numVisible: 1,
+        numScroll: 1,
+      },
     ];
-    
-    
-    
   }
 
   navigateToProductList(category: string) {
-    this.router.navigate(['/printers/list'], { queryParams: { categories: category } });
+    this.router.navigate(['/printers/list'], {
+      queryParams: { categories: category },
+    });
   }
 }
