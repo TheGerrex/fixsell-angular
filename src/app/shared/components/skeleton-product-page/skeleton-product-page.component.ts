c9532e3388604d53305a6b674c223ffb32d87a20
TheGerrex <gerrex95@gmail.com>
2025-02-20 18:21:25 -0600

    git reset

---
+++ src/app/shared/components/skeleton-product-page/skeleton-product-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-skeleton-product-page',
+  templateUrl: './skeleton-product-page.component.html',
+  styleUrls: ['./skeleton-product-page.component.scss']
+})
+export class SkeletonProductPageComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,26 +8,28 @@ import { ProductBrandPipe } from './pipes/product-brand.pipe';
 import { ProductColorPipe } from './pipes/product-color.pipe';
 import { ProductTypePipe } from './pipes/product-type.pipe';
 
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { SocialHeaderComponent } from './social-header/social-header.component';
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { ProductEmailFormComponent } from './components/product-email-form/product-email-form.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { SearchBarListComponent } from './components/search-bar-list/search-bar-list.component';
 import { SkeletonProductCardComponent } from './components/skeleton-product-card/skeleton-product-card.component';
+import { SkeletonProductPageComponent } from './components/skeleton-product-page/skeleton-product-page.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { PackageRentProductCardComponent } from './components/package-rent-product-card/package-rent-product-card.component';
 import { SoftwareEmailFormComponent } from './components/software-email-form/software-email-form.component';
 import { ContactEmailFormComponent } from './components/contact-email-form/contact-email-form.component';
 import { WhatsappButtonComponent } from './components/whatsapp-button/whatsapp-button.component';
+import { LocationSelectorComponent } from './components/location-selector/location-selector.component';
+import { LocationSelectorSocialHeaderComponent } from './components/location-selector-social-header/location-selector-social-header.component';
+
 import { SwiperDirective } from './directives/app-swiper.directive';
 
-// Step 1: Add the following line...
 import { register } from 'swiper/element/bundle';
-import { LocationSelectorComponent } from './location-selector/location-selector.component';
-import { LocationSelectorSocialHeaderComponent } from './components/location-selector-social-header/location-selector-social-header.component';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 
 // Step 2: Add the following line...
@@ -55,6 +57,7 @@ register();
     SwiperDirective,
     LocationSelectorComponent,
     LocationSelectorSocialHeaderComponent,
+    SkeletonProductPageComponent,
   ],
   imports: [
     CommonModule,
@@ -81,6 +84,7 @@ register();
     SoftwareEmailFormComponent,
     ContactEmailFormComponent,
     WhatsappButtonComponent,
+    SkeletonProductPageComponent,
     SwiperDirective
   ],
   providers: [provideNgxMask()],
